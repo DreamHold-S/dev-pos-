8aefafdaae48d37ed514e52b1c69cd81ddacb7ec
Shinebayr Ulambayr <shinee09992home@outlook.com>
2025-10-09 18:04:37 +0800

    Fixed or update

--- src/app/page.js
+++ src/app/page.js
@@ -119,7 +119,7 @@ export default function Home() {
         </div>
       </div>
       <div className="display: absolute bottom-5 text-white">
-        @copyright haha
+        @copyright CodePen LLC
       </div>
     </div>
   );
